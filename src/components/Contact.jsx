41d3c97c5c893579b094a738c2570ed2a69e6a68
Harish Lal <harishlal80@gmail.com>
2023-04-09 21:54:14 +0530

    email service added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,45 @@ const Contact = () => {
 	});
 	const [loading, setLoading] = useState(false);
 
-	const handleChange = (e) => {};
+	const handleChange = (e) => {
+		const { name, value } = e.target;
 
-	const handleSubmit = (e) => {};
+		setForm({ ...form, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+
+		emailjs
+			.send(
+				"service_xcawmdf",
+				"template_yfc4v39",
+				{
+					from_name: form.name,
+					to_name: "Harish Lal",
+					from_email: form.email,
+					to_email: "harishlal80@gmail.com",
+					message: form.message,
+				},
+				"U2dpoQMvHi4WruVyp"
+			)
+			.then(
+				() => {
+					setLoading(false);
+					alert(
+						"Thank You, I'll get back to you as soon as possible."
+					);
+
+					setForm({ name: "", email: "", message: "" });
+				},
+				(error) => {
+					setLoading(false);
+					console.log(error);
+					alert("Something went wrong!");
+				}
+			);
+	};
 
 	return (
 		<div className="xl:mt-12 xl:flex-row flwx-col-reverse flex gap-10 overflow-hidden">
