f29324ac4e8e2d30581fd756c64bad62ec8ae615
Harish Lal <harishlal80@gmail.com>
2023-05-07 19:09:55 +0530

    constants updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,16 +46,12 @@ const services = [
 		icon: web,
 	},
 	{
-		title: "React Native Developer",
+		title: "App Developer",
 		icon: mobile,
 	},
 	{
-		title: "Backend Developer",
+		title: "Photographer",
 		icon: backend,
-	},
-	{
-		title: "Content Creator",
-		icon: creator,
 	},
 ];
 
@@ -71,10 +67,6 @@ const technologies = [
 	{
 		name: "JavaScript",
 		icon: javascript,
-	},
-	{
-		name: "TypeScript",
-		icon: typescript,
 	},
 	{
 		name: "React JS",
@@ -93,24 +85,12 @@ const technologies = [
 		icon: nodejs,
 	},
 	{
-		name: "MongoDB",
-		icon: mongodb,
-	},
-	{
 		name: "Three JS",
 		icon: threejs,
 	},
 	{
 		name: "git",
 		icon: git,
-	},
-	{
-		name: "figma",
-		icon: figma,
-	},
-	{
-		name: "docker",
-		icon: docker,
 	},
 ];
 
@@ -134,32 +114,6 @@ const experiences = [
 		icon: tesla,
 		iconBg: "#E6DEDD",
 		date: "Jan 2021 - Feb 2022",
-		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
-		],
-	},
-	{
-		title: "Web Developer",
-		company_name: "Shopify",
-		icon: shopify,
-		iconBg: "#383E56",
-		date: "Jan 2022 - Jan 2023",
-		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
-		],
-	},
-	{
-		title: "Full stack Developer",
-		company_name: "Meta",
-		icon: meta,
-		iconBg: "#E6DEDD",
-		date: "Jan 2023 - Present",
 		points: [
 			"Developing and maintaining web applications using React.js and other related technologies.",
 			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
